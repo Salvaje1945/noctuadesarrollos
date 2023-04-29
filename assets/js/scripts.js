2e6fd3946a9a7ab49ab831ec40eb4834ae021500
nicolas <escriba.salvaje@gmail.com>
2023-04-29 05:26:49 -0300

    Carrousel de la primer sección de -contenido-, terminado.

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -16,3 +16,60 @@ function cabeceraCerrarMenu() {
 
 $('#cabecera-menu-abrir').onclick = cabeceraAbrirMenu
 $('#cabecera-menu-cerrar').onclick = cabeceraCerrarMenu
+
+function carrouselContenido() {
+    
+    let indice = 0
+
+    const tarjetas = document.querySelectorAll('.carrousel__slider--slide')
+
+    const anchoTarjetas = tarjetas[0].clientWidth
+
+    function tarjetasMovimiento() {
+
+        indice += 1
+
+        if(indice >= tarjetas.length){
+            indice = 0
+        }
+
+        $('#carrousel-slider').style.transform = "translateX(-" + indice * anchoTarjetas + "px)"
+
+        /*
+
+        let posicion = indice * anchoTarjetas
+
+        if(posicion === 0){
+            if($('#carrousel-slider').classList.contains('segundo')){
+                $('#carrousel-slider').classList.replace('segundo', 'primero')
+            }
+            if($('#carrousel-slider').classList.contains('tercero')){
+                $('#carrousel-slider').classList.replace('tercero', 'primero')
+            }
+        }
+
+        if(posicion === 1){
+            if($('#carrousel-slider').classList.contains('primero')){
+                $('#carrousel-slider').classList.replace('primero', 'segundo')
+            }
+            if($('#carrousel-slider').classList.contains('tercero')){
+                $('#carrousel-slider').classList.replace('tercero', 'segundo')
+            }
+        }
+
+        if(posicion === 2){
+            if($('#carrousel-slider').classList.contains('segundo')){
+                $('#carrousel-slider').classList.replace('segundo', 'tercer')
+            }
+            if($('#carrousel-slider').classList.contains('tercero')){
+                $('#carrousel-slider').classList.replace('tercero', 'primero')
+            }
+        }
+        */
+
+    }
+
+    setInterval(tarjetasMovimiento, 5000)
+}
+
+carrouselContenido()
